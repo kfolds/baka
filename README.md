a48a070466900afbcb377f41e185cfec1ab299aa
mia <mia+git@kfold.net>
2021-12-14 18:42:51 -0800

    some cleanup and refinements to lexer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# baka: a sussy programming language
+# baka: a programming language uwu
 
 ## example code
 
@@ -19,96 +19,11 @@ main :: (argc: int, argv: const u8[]) -> void {
 }
 ```
 
-## grammar
+## roadmap
 
-```
-Root:
-    Declaration+
-
-Declaration:
-    ( VarDecl
-    | AutoVarDecl
-    | ConstDecl
-    | AutoConstDecl
-    | FuncDecl
-    | StructDecl
-    | UnionDecl )
-
-Expr: Equality
-
-Equality: Comparison ( ( COND_NEQ | COND_EQ ) Comparison )*
-
-Comparison: Term ( ( COND_LT | COND_LEQ | COND_GT | COND_GEQ ) Term )*
-
-Term: Factor ( ( MINUS | PLUS ) Factor )*
-
-Factor: Unary ( ( FWD_SLASH | STAR | AT ) Unary )*
-
-Unary: ( ( COND_NOT | MINUS ) Unary | Primary )
-
-Primary:
-    ( Literal
-    | Identifier
-    | TRUE
-    | FALSE
-    | NULL
-    | PAREN_L Expr PAREN_R )
-
-Statement:
-    ( AssignExpr | Expr ) SEMI
-
-AssignExpr:
-    Identifier EQUALS Expr
-
-WhileStatement:
-    "while" PAREN_L Expr PAREN_R ( COLON PAREN_L AssignExpr PAREN_R )? Block
-
-ForStatement:
-    "for" PAREN_L Expr PAREN_R ( PIPE Identifier PIPE )? Block
-
-IfStatement:
-    "if" PAREN_L Expr PAREN_R Block ( ( "else" Block ) | ( "else" | IfStatement)+ )?
-
-SwitchBlock:
-    BRACE_L ( SwitchProng COMMA )* SwitchProng COMMA? BRACE_R
-
-SwitchProng:
-    ( ( Identifier | Literal | "else" ) | ( ( Identifier | Literal | "else" ) COMMA )+ )
-    BIG_ARROW ( Statement | Block )
-
-SwitchStatement:
-    "switch" PAREN_L Expr PAREN_R SwitchBlock
-
-BlockStatement:
-    ( WhileStatement | ForStatement | IfStatement | SwitchStatement )
-
-Block:
-    ( Declaration | Statement | BlockStatement )*
-
-VarDecl:
-    Identifier COLON Identifier (EQUALS Expr)? SEMI
-
-AutoVarDecl:
-    Identifier COLON_EQUALS Expr SEMI
-
-ConstDecl:
-    Identifier COLON Identifier COLON Expr SEMI
-
-AutoConstDecl:
-    Identifier DOUBLE_COLON Expr SEMI
-
-FuncSpecs:
-    INLINE | EXTERN
-
-ParamList: ( 
-    ParamDecl? | (ParamDecl COMMA)+ ParamDecl
-) 
-
-ParamDecl:
-    Identifier COLON Identifier (EQUALS Expr)?
-
-FuncDecl:
-    Identifier DOUBLE_COLON FuncSpecs* PAREN_L ParamList? PAREN_R ARROW Identifier Block
-
-
-```
+- [ ] *lexing and parsing basic language*
+- [ ] semantic analysis
+- [ ] codegen with LLVM
+- [ ] error unions/error sets
+- [ ] compile-time code execution
+- [ ] standard library
--- src/lexer.zig
+++ src/lexer.zig
@@ -5,6 +5,11 @@ const Allocator = std.mem.Allocator;
 
 const log = std.log.scoped(.lexer);
 
+const Error = error{
+    InvalidToken,
+    ReachedEof,
+};
+
 pub const TokenId = enum(u16) {
     identifier = 255,
     literal_int,
@@ -42,6 +47,9 @@ pub const TokenId = enum(u16) {
     kwd_and,
     kwd_or,
     kwd_error,
+    kwd_type,
+    kwd_fn,
+    kwd_const,
 
     op_assign,
     op_add,
@@ -95,7 +103,8 @@ pub const TokenId = enum(u16) {
 
 const Reserved = struct { src: []const u8, tag: TokenId };
 const reserved_atoms = .{
-    Reserved{ .src = "include", .tag = .kwd_include },
+    Reserved{ .src = "@import", .tag = .kwd_include },
+    Reserved{ .src = "type", .tag = .kwd_type },
     Reserved{ .src = "if", .tag = .kwd_if },
     Reserved{ .src = "else", .tag = .kwd_else },
     Reserved{ .src = "while", .tag = .kwd_while },
@@ -104,11 +113,13 @@ const reserved_atoms = .{
     Reserved{ .src = "continue", .tag = .kwd_continue },
     Reserved{ .src = "break", .tag = .kwd_break },
     Reserved{ .src = "return", .tag = .kwd_return },
+    Reserved{ .src = "fn", .tag = .kwd_fn },
     Reserved{ .src = "inline", .tag = .kwd_inline },
     Reserved{ .src = "extern", .tag = .kwd_extern },
     Reserved{ .src = "struct", .tag = .kwd_struct },
     Reserved{ .src = "union", .tag = .kwd_union },
     Reserved{ .src = "enum", .tag = .kwd_enum },
+    Reserved{ .src = "const", .tag = .kwd_const },
     //Reserved{ .src = "int", .tag = .kwd_int },
     //Reserved{ .src = "uint", .tag = .kwd_uint },
     //Reserved{ .src = "float", .tag = .kwd_float },
@@ -130,6 +141,7 @@ pub const Token = struct {
     line: usize = 0,
     col: usize = 0,
 
+    str: []const u8,
     data: ?Data = null,
 
     const Data = union {
@@ -142,14 +154,14 @@ pub const Token = struct {
 };
 
 fn match_op_or_sep(src: [:0]const u8, pos: usize) ?Token {
-    if (pos >= src.len) {
-        return null;
-    }
+    if (pos >= src.len) return null;
+
     const c = src[pos];
     const has_next = pos + 1 < src.len;
     const has_next_equals = has_next and src[pos + 1] == '=';
 
     var t: Token = undefined;
+    t.pos = pos;
     t.len = 1;
 
     switch (c) {
@@ -311,187 +323,129 @@ fn match_op_or_sep(src: [:0]const u8, pos: usize) ?Token {
         '\x00' => {
             t.tag = .end_of_file;
         },
-        else => {
-            return null;
-        },
+        else => return null,
     }
-    t.pos = pos;
+    t.str = src[pos .. pos + t.len];
     return t;
 }
 
-fn match_reserved(src: [:0]const u8, pos: usize, len: usize) ?Token {
-    inline for (reserved_atoms) |a| {
-        var l = a.src.len;
-        if (l == len and pos + l <= src.len and std.mem.eql(u8, a.src, src[pos .. pos + l])) {
-            return Token{ .tag = a.tag, .len = a.src.len, .pos = pos, .data = null };
-        }
-    }
-    return null;
+fn is_identifier_char(c: u8) bool {
+    return std.ascii.isAlNum(c) or c == '_';
 }
 
-fn match_identifier_chars(src: [:0]const u8, pos: usize) bool {
-    return (std.ascii.isAlNum(src[pos]) or src[pos] == '_');
+/// match integer literals prefixed with `0b`, `0o`, or `0x` for binary, octal, and hexadecimal literals, respectively.
+fn match_int_literal(src: [:0]const u8, pos: usize) !?Token {
+    if (src[pos] != '0' or pos + 2 >= src.len) return null;
+    var token: Token = undefined;
+    token.tag = switch (src[pos + 1]) {
+        'b' => .literal_bin,
+        'o' => .literal_oct,
+        'x' => .literal_hex,
+        else => return null,
+    };
+    token.pos = pos;
+    token.len = 2;
+
+    while (pos + token.len < src.len and is_identifier_char(src[pos + token.len])) token.len += 1;
+
+    token.data = Token.Data{ .uint_value = try std.fmt.parseUnsigned(u128, src[pos .. pos + token.len], 0) };
+    token.str = src[pos .. pos + token.len];
+    return token;
 }
 
-/// match literally any kind of numeric literal
-/// 
-/// TODO(mia): this is so ugly
+/// match literal integers and floats.
 fn match_numeric_literal(src: [:0]const u8, pos: usize) !?Token {
+    if (try match_int_literal(src, pos)) |t| return t;
+
     const has_next = pos + 1 < src.len;
-    if ((!std.ascii.isDigit(src[pos]) and src[pos] != '.') or (has_next and src[pos] == '.' and !std.ascii.isDigit(src[pos + 1]))) {
+    var seen_dot = src[pos] == '.';
+    if ((!std.ascii.isDigit(src[pos]) and !seen_dot) or (has_next and seen_dot and !std.ascii.isDigit(src[pos + 1]))) {
         return null;
     }
-    var token: ?Token = null;
-    var token_len: usize = 1;
-    const c = src[pos];
-    if (has_next) {
-        const next_c = src[pos + 1];
-        if (c == '.' and std.ascii.isDigit(next_c)) {
-            // handle float literal
-            var seen_e = false;
-            var seen_neg = false;
-            while (pos + token_len < src.len and (match_identifier_chars(src, pos + token_len) or (seen_e and !seen_neg and src[pos + token_len] == '-'))) {
-                seen_e = seen_e or src[pos + token_len] == 'e';
-                seen_neg = seen_neg or src[pos + token_len] == '-';
-                token_len += 1;
-            }
-            const value = try std.fmt.parseFloat(f64, src[pos .. pos + token_len]);
-            token = Token{
-                .tag = .literal_float,
-                .pos = pos,
-                .len = token_len,
-                .data = Token.Data{ .f64_value = value },
-            };
-        } else if (c == '0' and next_c == 'x') {
-            // handle hexadecimal literal
-            token_len += 1;
-            while (pos + token_len < src.len and std.ascii.isAlNum(src[pos + token_len])) { // ((src[pos + token_len] >= 'a' and src[pos + token_len] <= 'f') or (src[pos + token_len] >= 'A' and src[pos + token_len] <= 'F') or (src[pos + token_len] >= '0' and src[pos + token_len] <= '9'))) {
-                token_len += 1;
-            }
-            const value = try std.fmt.parseUnsigned(u128, src[pos .. pos + token_len - 1], 0);
-            token = Token{
-                .tag = .literal_hex,
-                .pos = pos,
-                .len = token_len,
-                .data = Token.Data{ .uint_value = value },
-            };
-        } else if (c == '0' and next_c == 'b') {
-            // handle binary literal
-            token_len += 1;
-            while (pos + token_len < src.len and match_identifier_chars(src, pos + token_len)) { // (src[pos + token_len] == '0' or src[pos + token_len] == '1')) {
-                token_len += 1;
-            }
-            //std.debug.warn("{s}\n", .{src[pos + 2 .. pos + 2 + token_len]});
-            const value = try std.fmt.parseUnsigned(u128, src[pos .. pos + token_len - 1], 0);
-            token = Token{
-                .tag = .literal_bin,
-                .pos = pos,
-                .len = token_len,
-                .data = Token.Data{ .uint_value = value },
-            };
-        } else if (c == '0' and next_c == 'o') {
-            // handle octal literal
-            token_len += 1;
-            while (pos + token_len < src.len and match_identifier_chars(src, pos + token_len)) { // (src[pos + token_len] == '0' or src[pos + token_len] == '1')) {
-                token_len += 1;
-            }
-            //std.debug.warn("{s}\n", .{src[pos + 2 .. pos + 2 + token_len]});
-            const value = try std.fmt.parseUnsigned(u128, src[pos .. pos + token_len - 1], 0);
-            token = Token{
-                .tag = .literal_oct,
-                .pos = pos,
-                .len = token_len,
-                .data = Token.Data{ .uint_value = value },
-            };
-        } else {
-            // handle numeric literal (could be int or float)
-            var seen_dot = false;
-            var seen_e = false;
-            var seen_neg = false;
-            while (pos + token_len < src.len and (match_identifier_chars(src, pos + token_len) or src[pos + token_len] == '.' or (seen_e and !seen_neg and src[pos + token_len] == '-'))) {
-                seen_dot = seen_dot or src[pos + token_len] == '.';
-                seen_e = seen_e or src[pos + token_len] == 'e';
-                seen_neg = seen_neg or src[pos + token_len] == '-';
-                token_len += 1;
-            }
-            if (seen_dot or seen_e) {
-                // handle float literal
-                const value = try std.fmt.parseFloat(f64, src[pos .. pos + token_len]);
-                token = Token{
-                    .tag = .literal_float,
-                    .pos = pos,
-                    .len = token_len,
-                    .data = Token.Data{ .f64_value = value },
-                };
-            } else {
-                // handle integer literal
-                const value = try std.fmt.parseUnsigned(u128, src[pos .. pos + token_len], 10);
-                token = Token{
-                    .tag = .literal_int,
-                    .pos = pos,
-                    .len = token_len,
-                    .data = Token.Data{ .uint_value = value },
-                };
-            }
+    var token: Token = undefined;
+    token.pos = pos;
+    token.len = 0;
+
+    seen_dot = false;
+    var seen_e = false;
+
+    while (pos + token.len < src.len) {
+        switch (src[pos + token.len]) {
+            '.' => if (seen_dot or seen_e) return Error.InvalidToken,
+            'e', 'E' => if (seen_e) return Error.InvalidToken,
+            '-' => {
+                const prev_char = src[pos + token.len - 1];
+                if (!seen_e or (prev_char != 'e' and prev_char != 'E')) break;
+            },
+            '_', '0'...'9' => {},
+            'a'...'d', 'f'...'z', 'A'...'D', 'F'...'Z' => return Error.InvalidToken,
+            else => break,
         }
-    } else {
-        if (std.ascii.isDigit(c)) {
-            // handle single-digit number that is probably an error??
-            const value = try std.fmt.parseUnsigned(u128, src[pos .. pos + token_len], 10);
-            token = Token{
-                .tag = .literal_int,
-                .pos = pos,
-                .len = token_len,
-                .data = Token.Data{ .uint_value = value },
-            };
-        }
+        seen_dot = seen_dot or src[pos + token.len] == '.';
+        seen_e = seen_e or src[pos + token.len] == 'e' or src[pos + token.len] == 'E';
+
+        token.len += 1;
     }
+
+    token.tag = if (seen_dot or seen_e) .literal_float else .literal_int;
+    token.data = if (token.tag == .literal_float) .{
+        .f64_value = try std.fmt.parseFloat(f64, src[pos .. pos + token.len]),
+    } else .{
+        .uint_value = try std.fmt.parseUnsigned(u128, src[pos .. pos + token.len], 10),
+    };
+    token.str = src[pos .. pos + token.len];
 
     return token;
 }
 
-fn match_string_literal(src: [:0]const u8, pos: usize) !?Token {
-    if (pos >= src.len or src[pos] != '"') {
-        return null;
-    }
+fn match_string_or_char_literal(src: [:0]const u8, pos: usize) !?Token {
+    if (pos >= src.len or (src[pos] != '"' and src[pos] != '\'')) return null;
+    var token: Token = undefined;
+    token.pos = pos;
+    token.len = 1;
 
-    var token_len: usize = 1;
-    while (pos + token_len < src.len and src[pos + token_len] != '"') {
-        token_len += @as(usize, 1) + @boolToInt(src[pos + token_len] == '\\');
-    }
+    const end_char = src[pos];
+    token.tag = if (end_char == '"') .literal_string else .literal_char;
 
-    if (src[pos + token_len] != '"') {
-        return error.ReachedEof;
+    while (pos + token.len < src.len and src[pos + token.len] != end_char) {
+        token.len += 1 + @intCast(usize, @boolToInt(src[pos + token.len] == '\\'));
     }
+    if (src[pos + token.len] != end_char) return Error.ReachedEof;
 
-    return Token{
-        .tag = .literal_string,
-        .len = token_len + 1,
-        .pos = pos,
-        .data = Token.Data{ .str_value = src[pos .. pos + token_len + 1] },
-    };
+    token.len += 1;
+    token.data = Token.Data{ .str_value = src[pos .. pos + token.len] };
+    token.str = src[pos .. pos + token.len];
+
+    return token;
 }
 
-fn match_char_literal(src: [:0]const u8, pos: usize) !?Token {
-    if (pos >= src.len or src[pos] != '\'') {
-        return null;
+fn match_identifier_or_kwd(src: [:0]const u8, pos: usize) ?Token {
+    // zig fmt: off
+    if (
+        (pos >= src.len or (!std.ascii.isAlpha(src[pos]) and src[pos] != '_'))
+        or (pos + 1 < src.len and src[pos] == '@' and !std.ascii.isAlpha(src[pos + 1])) 
+    ) return null;
+    // zig fmt: on
+
+    var token: Token = undefined;
+    token.pos = pos;
+    token.len = 1;
+
+    while (pos + token.len < src.len and is_identifier_char(src[pos + token.len])) {
+        token.len += 1;
+    }
+    token.str = src[pos .. pos + token.len];
+
+    // TODO(mia): use comptime string hash map
+    token.tag = .identifier;
+    inline for (reserved_atoms) |a| {
+        if (a.src.len == token.len and std.mem.eql(u8, a.src, token.str)) {
+            token.tag = a.tag;
+            break;
+        }
     }
 
-    var token_len: usize = 1;
-    while (pos + token_len < src.len and src[pos + token_len] != '\'') {
-        token_len += 1 + @boolToInt(src[pos + token_len] == '\\');
-    }
-
-    if (src[pos + token_len] != '\'') {
-        return error.ReachedEof;
-    }
-
-    return Token{
-        .tag = .literal_string,
-        .len = token_len + 1,
-        .pos = pos,
-        .data = Token.Data{ .str_value = src[pos .. pos + token_len] },
-    };
+    return token;
 }
 
 pub const Lexer = struct {
@@ -505,12 +459,12 @@ pub const Lexer = struct {
         var pos = l.pos;
         var line = l.cur_line;
         var col = l.cur_col;
-        var token_len: usize = 1;
         const src = l.src;
 
-        var token = Token{ .tag = .end_of_file, .pos = pos, .len = token_len, .data = null };
+        var token = Token{ .tag = .end_of_file, .pos = pos, .len = 1, .data = null, .str = "" };
 
         while (pos < src.len) {
+            // skip whitespace
             while (pos < src.len and std.ascii.isSpace(src[pos])) {
                 if (src[pos] == '\n') {
                     line += 1;
@@ -521,9 +475,8 @@ pub const Lexer = struct {
                 pos += 1;
             }
 
-            if (pos >= src.len) break;
-
-            if (pos < src.len - 1 and src[pos] == '/' and src[pos + 1] == '/') {
+            // skip comments
+            if (pos + 1 < src.len and src[pos] == '/' and src[pos + 1] == '/') {
                 while (pos < src.len and src[pos] != '\n') {
                     pos += 1;
                 }
@@ -534,42 +487,23 @@ pub const Lexer = struct {
 
             if (pos >= src.len) break;
 
-            if (std.ascii.isAlpha(src[pos]) or src[pos] == '_') {
-                while (pos + token_len < src.len and match_identifier_chars(src, pos + token_len)) {
-                    token_len += 1;
-                }
-                if (match_reserved(src, pos, token_len)) |t| {
-                    token.tag = t.tag;
-                } else {
-                    token.tag = .identifier;
-                }
-                break;
-            }
-
             if (try match_numeric_literal(src, pos)) |t| {
-                token.tag = t.tag;
-                token.data = t.data;
-                token_len = t.len;
+                token = t;
                 break;
             }
 
-            if (try match_string_literal(src, pos)) |t| {
-                token.tag = t.tag;
-                token.data = t.data;
-                token_len = t.len;
+            if (try match_string_or_char_literal(src, pos)) |t| {
+                token = t;
                 break;
             }
 
-            if (try match_char_literal(src, pos)) |t| {
-                token.tag = t.tag;
-                token.data = t.data;
-                token_len = t.len;
+            if (match_identifier_or_kwd(src, pos)) |t| {
+                token = t;
                 break;
             }
 
             if (match_op_or_sep(src, pos)) |t| {
-                token.tag = t.tag;
-                token_len = t.len;
+                token = t;
                 break;
             }
 
@@ -577,14 +511,12 @@ pub const Lexer = struct {
             col += 1;
         }
 
-        token.len = token_len;
-        token.pos = pos;
         token.line = line;
         token.col = col;
 
-        l.pos = pos + token_len;
+        l.pos = pos + token.len;
         l.cur_line = line;
-        l.cur_col = col + token_len;
+        l.cur_col = col + token.len;
 
         return token;
     }
@@ -610,7 +542,7 @@ pub fn tokenize(allocator: *Allocator, src: [:0]const u8) ![]Token {
 
 test "basic lexing" {
     const src =
-        \\include rand;
+        \\rand :: @import("std").rand;
         \\foo :: inline (x: int) -> int {
         \\    y := rand();
         \\    if (y < 0.5) {
@@ -625,6 +557,7 @@ test "basic lexing" {
     var l = Lexer{ .src = src };
     while (l.pos <= l.src.len) {
         const t = try l.get_next_token();
+        std.debug.warn("{}\n", .{t});
     }
 }
 
@@ -654,6 +587,7 @@ fn assert_match(src: [:0]const u8, tags: []const TokenId) !void {
 
     while (l.pos < l.src.len and i < tags.len) : (i += 1) {
         const t = try l.get_next_token();
+        std.debug.warn("{}\n", .{t});
         if (t.tag != tags[i]) {
             std.debug.panic("expected {s}, got {s}\n", .{ tags[i], t.tag });
         }
@@ -661,8 +595,10 @@ fn assert_match(src: [:0]const u8, tags: []const TokenId) !void {
     if (i == tags.len) std.debug.panic("reached end of string before finishing\n", .{});
 }
 
-test "lex numeric literals" {
-    const src = "0 1 1 0b10 0x29a 0o400 1.3 0.1 .32 1e2 9.42e-3";
+test "lex literals" {
+    const src =
+        \\0 1 1 0b10 0x29a 0o400 1.3 0.1 .32 1e2 9.42e-3 "hello\n" '0'
+    ;
     const tags = &[_]TokenId{
         .literal_int,
         .literal_int,
@@ -675,6 +611,30 @@ test "lex numeric literals" {
         .literal_float,
         .literal_float,
         .literal_float,
+        .literal_string,
+        .literal_char,
+        .end_of_file,
+    };
+
+    try assert_match(src, tags[0..]);
+}
+
+test "lex identifiers" {
+    const src =
+        \\foo foo0 foo_ _foo _foo0 _foo _foo0_ foo_0 _foo_0_ _foo_0 foo_0_
+    ;
+    const tags = &[_]TokenId{
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
+        .identifier,
         .end_of_file,
     };
 
